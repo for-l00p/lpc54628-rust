2c01a91dc7cbc7f668dd33c4a1abae1345a63ffd
Kook Dong-Min <pklazy@gmail.com>
2019-08-13 19:49:55 +0900

    add enable_sram, semihosting config

--- examples/hello.rs
+++ examples/hello.rs
@@ -3,9 +3,17 @@
 
 extern crate panic_halt;
 
-use cortex_m_rt::entry;
+use cortex_m_rt::{entry, pre_init};
+use cortex_m_semihosting::hprintln;
+
+#[pre_init]
+unsafe fn enable_sram() {
+    *(0x4000_0220 as *mut u32) = 0x38;
+}
 
 #[entry]
 fn main() -> ! {
+    hprintln!("Hello, world!").unwrap();
+
     loop {}
 }
--- jlink.gdb
+++ jlink.gdb
@@ -1,4 +1,7 @@
 target remote :2331
+monitor semihosting enable
+monitor semihosting breakOnError
+monitor semihosting IOclient 2
 load
 break main
 continue
